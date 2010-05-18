b49c316dd6b69dd05781e33ca769ad8a31ffd0ae
Adam Koprowski <Adam.Koprowski@mlstate.com>
2010-05-18 14:44:14 +0200

    Reformulated [check_n] and its properties proven.

--- nat_util.v
+++ nat_util.v
@@ -2,6 +2,7 @@ Require Export Arith.
 Require Export Peano_dec.
 Require Import util.
 Require Import EqdepFacts.
+Require Import tactics.
 
 Set Implicit Arguments.
 
@@ -61,46 +62,78 @@ Section Check_n.
 
   Variables 
     (n : nat) 
-    (P : forall i, i < n -> Prop).
+    (P Q : forall i, i < n -> Prop).
 
-  Program Fixpoint check_n_aux (p : nat | p <= n) 
-    {measure (fun p => n - p) p} : Prop :=
+  Obligation Tactic := crunch.
 
-    match le_lt_dec n p with
-    | left _ => True
-    | right cmp =>
-        @P p cmp /\ @check_n_aux (S p)
+  Program Fixpoint check_n_aux (p : nat) (H : p < n) : Prop :=
+    let this := @P p H in
+    let rest := 
+      match p with
+      | 0 => True
+      | S p' => @check_n_aux p' _
+      end
+    in
+    this /\ rest.
+
+  Program Definition check_n := 
+    match n with
+    | 0 => True
+    | _ => @check_n_aux (pred n) _
     end.
 
-  Next Obligation.
+  Variable P_holds : forall i (ip: i < n), P ip.
+
+  Lemma check_n_holds_aux : 
+    forall i (ip : i < n), check_n_aux ip.
   Proof.
-    omega.
+    induction i; crunch.
   Qed.
-
-  Program Definition check_n := @check_n_aux 0.
-
-  Next Obligation.
-  Proof.
-    omega.
-  Qed.
-
-  Lemma check_n_holds : 
-    (forall i (ip: i < n), P ip) -> check_n.
-  Proof.
-  Admitted.
 
 End Check_n.
+
+Lemma check_n_holds n (P : forall i, i < n -> Prop) : 
+  (forall i (ip: i < n), P i ip) -> check_n P.
+Proof.
+  destruct n; crunch.
+  apply check_n_holds_aux; crunch.
+Qed.
 
 Section Check_n_equiv.
 
   Variables 
     (n : nat)
-    (P Q : forall i, i < n -> Prop).
+    (P Q : forall i, i < n -> Prop)
+    (P_eq_Q : forall i (ip : i < n), P ip <-> Q ip).
 
-  Lemma check_n_equiv : 
-    (forall i (ip : i < n), P ip <-> Q ip) ->
-    (check_n P <-> check_n Q).
+  Lemma check_n_equiv_aux : 
+    forall i (ip : i < n), 
+      check_n_aux P ip <-> check_n_aux Q ip.
   Proof.
-  Admitted.
+     (* FIXME, there must be a better way to state this proof, without 
+        those asserts (which essentially break iff into two implications *)
+    pose (fun i (ip : i < n) => proj1 (P_eq_Q ip)).
+    pose (fun i (ip : i < n) => proj2 (P_eq_Q ip)).
+    induction i.
+    crunch.
+    pose (fun ip : i < n => proj1 (IHi ip)).
+    pose (fun ip : i < n => proj2 (IHi ip)).
+    crunch.
+  Qed.
 
 End Check_n_equiv.
+
+Lemma check_n_equiv n 
+  (P Q : forall i, i < n -> Prop)
+  (P_eq_Q : forall i (ip : i < n), P i ip <-> Q i ip) :
+  check_n P <-> check_n Q.
+Proof.
+  destruct n; intros.
+  crunch.
+  unfold check_n.
+  match goal with
+    | |- check_n_aux _ ?ip <-> check_n_aux _ ?jp =>
+      rewrite (lt_unique ip jp)
+  end.
+  apply check_n_equiv_aux. trivial.
+Qed.
